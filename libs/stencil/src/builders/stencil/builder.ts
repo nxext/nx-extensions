e1333d7b59c01e2330d36d0cf75ad4cc6ad5530a
Dominik Pieper <dominik@pieper.io>
2020-05-09 13:39:21 +0200

    refactor(nxext): use external stencil functions (just run with parameters)

--- libs/stencil/src/builders/stencil/builder.ts
+++ libs/stencil/src/builders/stencil/builder.ts
@@ -6,18 +6,60 @@ import {
 import { Observable } from 'rxjs';
 import { StencilBuilderOptions } from './schema';
 import {
+  createNodeLogger, createNodeSystem,
+  run,
   TaskCommand
 } from '@stencil/core/cli';
-import { createStencilConfig, createStencilProcess } from '../core/stencilCompiler';
+import { projectRootDir } from '@nrwl/workspace';
+
+function createStencilCompilerOptions(
+  taskCommand: TaskCommand,
+  options: StencilBuilderOptions,
+  context: BuilderContext
+): string[] {
+  const projectDir = projectRootDir(options.projectType);
+  const runOptions = [
+    '',
+    '',
+    'build',
+    '--config',
+    `${projectDir}/${context.target.project}/stencil.config.ts`
+  ];
+  if (options.port) {
+    runOptions.push(`--port ${options.port}`);
+  }
+
+  Object.keys(options).forEach((optionKey) => {
+    if (typeof options[optionKey] === 'boolean' && options[optionKey]) {
+      runOptions.push(`--${optionKey}`);
+    }
+  });
+
+  return runOptions;
+}
+
+export function createStencilProcessOriginalRun(taskCommand: TaskCommand,
+                                                options: StencilBuilderOptions,
+                                                context: BuilderContext) {
+  return new Observable<BuilderOutput>((obs) => {
+    process.argv = createStencilCompilerOptions(taskCommand, options, context);
+
+    run({
+      process: process,
+      logger: createNodeLogger(process),
+      sys: createNodeSystem(process)
+    })
+      .then(() => obs.next({ success: true }))
+      .catch(err => obs.error(err));
+  });
+}
 
 export function runBuilder(
   options: StencilBuilderOptions,
   context: BuilderContext
 ): Observable<BuilderOutput> {
   const taskCommand: TaskCommand = 'build';
-  return createStencilConfig(taskCommand, options, context).pipe(
-    createStencilProcess()
-  );
+  return createStencilProcessOriginalRun(taskCommand, options, context);
 }
 
 export default createBuilder(runBuilder);
