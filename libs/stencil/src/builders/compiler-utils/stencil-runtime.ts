2d6502c1209d5adac4c5da59635e6fa006e636d6
Dominik Pieper <dominik@pieper.io>
2020-05-30 00:26:41 +0200

    feat(nxext): add the ability to change the stencil.config.ts file path

--- libs/stencil/src/builders/compiler-utils/stencil-runtime.ts
+++ libs/stencil/src/builders/compiler-utils/stencil-runtime.ts
@@ -5,7 +5,7 @@ import {
   createNodeSystem,
   Logger,
   runTask,
-  TaskCommand
+  TaskCommand,
 } from '@stencil/core/cli';
 import { StencilBuildOptions } from '../build/schema';
 import { BuilderContext, BuilderOutput } from '@angular-devkit/architect';
@@ -16,7 +16,6 @@ import { map, switchMap } from 'rxjs/operators';
 import { StencilTestOptions } from '../test/schema';
 import { StencilE2EOptions } from '../e2e/schema';
 
-
 function getCompilerExecutingPath() {
   return require.resolve('@stencil/core/compiler');
 }
@@ -25,10 +24,18 @@ export function createStencilConfig(
   taskCommand: TaskCommand,
   options: StencilBuildOptions | StencilTestOptions,
   context: BuilderContext,
-  createStencilCompilerOptions: (taskCommand: TaskCommand, options: StencilBuildOptions) => ConfigFlags
+  createStencilCompilerOptions: (
+    taskCommand: TaskCommand,
+    options: StencilBuildOptions
+  ) => ConfigFlags
 ): Observable<Config> {
   const projectDir = projectRootDir(options.projectType);
-  const configFilePath = `${projectDir}/${context.target.project}/stencil.config.ts`;
+
+  let configFilePath = `${projectDir}/${context.target.project}/stencil.config.ts`;
+
+  if (options.configPath) {
+    configFilePath = options.configPath;
+  }
 
   const flags = createStencilCompilerOptions(taskCommand, options);
   const logger: Logger = createNodeLogger(process);
@@ -45,19 +52,17 @@ export function createStencilConfig(
   return from(
     loadConfig({
       config: {
-        flags
+        flags,
       },
       configPath: configFilePath,
       logger,
-      sys
+      sys,
     })
-  ).pipe(
-    map((loadConfigResults) => loadConfigResults.config)
-  );
+  ).pipe(map((loadConfigResults) => loadConfigResults.config));
 }
 
 export function createStencilProcess() {
-  return function(source: Observable<Config>): Observable<BuilderOutput> {
+  return function (source: Observable<Config>): Observable<BuilderOutput> {
     return source.pipe(
       switchMap(
         (config) =>
@@ -71,7 +76,10 @@ export function createStencilProcess() {
   };
 }
 
-export function parseRunParameters(runOptions: string[], options: StencilBuildOptions | StencilTestOptions | StencilE2EOptions) {
+export function parseRunParameters(
+  runOptions: string[],
+  options: StencilBuildOptions | StencilTestOptions | StencilE2EOptions
+) {
   Object.keys(options).forEach((optionKey) => {
     if (typeof options[optionKey] === 'boolean' && options[optionKey]) {
       runOptions.push(`--${optionKey}`);
