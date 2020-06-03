c47e83155437f92aa408aacf568a4305c3def460
Dominik Pieper <dominik@pieper.io>
2020-06-03 12:59:49 +0200

    test(nxext): add e2e file for the configPath feature

---
+++ apps/stencil-e2e/tests/builderoptions.test.ts
@@ -0,0 +1,48 @@
+import {
+  ensureNxProject,
+  renameFile,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+
+describe('e2e', () => {
+  describe('stencil builder', () => {
+    it(`should build with custom stencil config naming`, async (done) => {
+      const plugin = uniq('library');
+      ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
+
+      await runNxCommandAsync(
+        `generate @nxext/stencil:lib ${plugin} --style=scss`
+      );
+
+      const uniqConfigFilename = uniq('stencil.config');
+      renameFile(
+        `libs/${plugin}/stencil.config.ts`,
+        `libs/${plugin}/${uniqConfigFilename}.ts`
+      );
+
+      const result = await runNxCommandAsync(
+        `build ${plugin} --configPath=libs/${plugin}/${uniqConfigFilename}.ts`
+      );
+      expect(result.stdout).toContain('build finished');
+
+      done();
+    });
+
+    it(`should build with custom configPath`, async (done) => {
+      const plugin = uniq('library');
+      ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
+
+      await runNxCommandAsync(
+        `generate @nxext/stencil:lib ${plugin} --style=scss`
+      );
+
+      const result = await runNxCommandAsync(
+        `build ${plugin} --configPath=libs/${plugin}/stencil.config.ts`
+      );
+      expect(result.stdout).toContain('build finished');
+
+      done();
+    });
+  });
+});
