1b6db1aed8bdc33ce6889d4cdd6ebdc178c2aa86
Dominik Pieper <dominik@pieper.io>
2020-06-11 08:44:11 +0200

    refactor(nxext): fix lint checks

--- libs/stencil/src/schematics/storybook/configuration/configuration.ts
+++ libs/stencil/src/schematics/storybook/configuration/configuration.ts
@@ -1,26 +1,11 @@
 import {
-  applyTemplates,
   chain,
   externalSchematic,
-  move,
-  noop,
   Rule,
   schematic,
-  SchematicContext,
-  template,
-  Tree,
-  url,
 } from '@angular-devkit/schematics';
-import {
-  getProjectConfig,
-  offsetFromRoot,
-  updateWorkspace,
-} from '@nrwl/workspace';
-import { join, normalize } from '@angular-devkit/core';
 import { StorybookConfigureSchema } from './schema';
-import { toJS } from '@nrwl/workspace/src/utils/rules/to-js';
 import { CypressConfigureSchema } from '@nrwl/storybook/src/schematics/cypress-project/cypress-project';
-import { applyWithSkipExisting, parseJsonAtPath } from '../../../utils/utils';
 
 export default function (options: StorybookConfigureSchema): Rule {
   return chain([
