a9c8be64a4f30d1ed13ccfa6dfab1160e1621ff8
Dominik Pieper <dominik@pieper.io>
2020-06-17 10:03:43 +0200

    test(nxext): clean up some e2e tests and reduce number of tests, should be testet in unit tests

--- apps/stencil-e2e/tests/ionic-pwa.test.ts
+++ apps/stencil-e2e/tests/ionic-pwa.test.ts
@@ -6,80 +6,75 @@ import {
   uniq,
 } from '@nrwl/nx-plugin/testing';
 import { testProject } from '../utils/testing';
-import { SUPPORTED_STYLE_LIBRARIES } from '../../../libs/stencil/src/utils/testing';
 
 describe('e2e-pwa', () => {
   describe('stencil pwa', () => {
-    for (var style of ['css', 'scss', 'less', 'styl', 'pcss']) {
-      xit(`should create pwa with ${style}`, async (done) => {
-        const plugin = uniq('pwa');
+    it(`should create pwa with css`, async (done) => {
+      const plugin = uniq('pwa');
 
-        ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
-        await runNxCommandAsync(
-          `generate @nxext/stencil:pwa ${plugin} --style=${style}`
-        );
+      ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
+      await runNxCommandAsync(
+        `generate @nxext/stencil:pwa ${plugin} --style='css'`
+      );
 
-        testProject(plugin, style, ProjectType.Application);
+      testProject(plugin, 'css', ProjectType.Application);
 
-        done();
-      });
-    }
-
-    describe('--directory', () => {
-      it('should create src in the specified directory', async (done) => {
-        const plugin = uniq('pwa');
-        ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
-        await runNxCommandAsync(
-          `generate @nxext/stencil:pwa ${plugin} --directory subdir --style=css`
-        );
-        expect(() =>
-          checkFilesExist(`apps/subdir/${plugin}/stencil.config.ts`)
-        ).not.toThrow();
-        done();
-      });
+      done();
     });
+  });
 
-    describe('--tags', () => {
-      it('should add tags to nx.json', async (done) => {
-        const plugin = uniq('pwa');
-        ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
-        await runNxCommandAsync(
-          `generate @nxext/stencil:pwa ${plugin} --tags e2etag,e2ePackage --style=css`
-        );
-
-        expect(() => {
-          checkFilesExist(
-            `${projectRootDir(
-              ProjectType.Application
-            )}/${plugin}/src/components/app-root/app-root.e2e.ts`,
-            `${projectRootDir(
-              ProjectType.Application
-            )}/${plugin}/src/components/app-profile/app-profile.e2e.ts`,
-            `${projectRootDir(
-              ProjectType.Application
-            )}/${plugin}/src/components/app-home/app-home.e2e.ts`
-          );
-        }).not.toThrow();
-
-        done();
-      });
+  describe('--directory', () => {
+    it('should create src in the specified directory', async (done) => {
+      const plugin = uniq('pwa');
+      ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
+      await runNxCommandAsync(
+        `generate @nxext/stencil:pwa ${plugin} --directory subdir --style=css`
+      );
+      expect(() =>
+        checkFilesExist(`apps/subdir/${plugin}/stencil.config.ts`)
+      ).not.toThrow();
+      done();
     });
+  });
 
-    describe('stencil app builder', () => {
-      SUPPORTED_STYLE_LIBRARIES.forEach((style) => {
-        it(`should bould pwa app with ${style}`, async (done) => {
-          const plugin = uniq('pwa');
-          ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
-          await runNxCommandAsync(
-            `generate @nxext/stencil:pwa ${plugin} --style=${style}`
-          );
+  describe('--tags', () => {
+    it('should add tags to nx.json', async (done) => {
+      const plugin = uniq('pwa');
+      ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
+      await runNxCommandAsync(
+        `generate @nxext/stencil:pwa ${plugin} --tags e2etag,e2ePackage --style=css`
+      );
 
-          const result = await runNxCommandAsync(`build ${plugin} --dev`);
-          expect(result.stdout).toContain('build finished');
+      expect(() => {
+        checkFilesExist(
+          `${projectRootDir(
+            ProjectType.Application
+          )}/${plugin}/src/components/app-root/app-root.e2e.ts`,
+          `${projectRootDir(
+            ProjectType.Application
+          )}/${plugin}/src/components/app-profile/app-profile.e2e.ts`,
+          `${projectRootDir(
+            ProjectType.Application
+          )}/${plugin}/src/components/app-home/app-home.e2e.ts`
+        );
+      }).not.toThrow();
 
-          done();
-        });
-      });
+      done();
+    });
+  });
+
+  describe('stencil app builder', () => {
+    it(`should bould pwa app with scss`, async (done) => {
+      const plugin = uniq('pwa');
+      ensureNxProject('@nxext/stencil', 'dist/libs/stencil');
+      await runNxCommandAsync(
+        `generate @nxext/stencil:pwa ${plugin} --style='scss'`
+      );
+
+      const result = await runNxCommandAsync(`build ${plugin} --dev`);
+      expect(result.stdout).toContain('build finished');
+
+      done();
     });
   });
 });
