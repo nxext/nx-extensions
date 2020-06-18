0beaee5df28936c80cb8408a92f91cf27ea13dd1
Dominik Pieper <dominik@pieper.io>
2020-06-18 16:56:01 +0200

    test(nxext): refactor migration test

--- libs/stencil/src/migrations/update-0-1-0/update-0-1-0.spec.ts
+++ libs/stencil/src/migrations/update-0-1-0/update-0-1-0.spec.ts
@@ -1,6 +1,6 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import { readJsonInTree } from '@nrwl/workspace';
+import { readJsonInTree, serializeJson } from '@nrwl/workspace';
 import * as path from 'path';
 import {
   createTestUILib,
@@ -22,10 +22,111 @@ describe('update-0-1-0', () => {
   (SUPPORTED_STYLE_LIBRARIES as SupportedStyles[]).forEach((style) => {
     it(`should update component schematics config with ${style}`, async () => {
       initialTree = await createTestUILib('library', style);
-      const workspaceJson = readJsonInTree(initialTree, '/workspace.json');
-      delete workspaceJson.projects.library.schematics;
-
-      initialTree.overwrite('/workspace.json', JSON.stringify(workspaceJson));
+      initialTree.overwrite(
+        'workspace.json',
+        serializeJson({
+          version: 1,
+          projects: {
+            testlib: {
+              projectType: 'library',
+              root: 'libs/testlib',
+              sourceRoot: 'libs/testlib/src',
+              architect: {
+                build: {
+                  builder: '@nxext/stencil:build',
+                  options: {
+                    projectType: 'library',
+                    configPath: 'libs/testlib/stencil.config.ts',
+                  },
+                },
+                test: {
+                  builder: '@nxext/stencil:test',
+                  options: {
+                    projectType: 'library',
+                    configPath: 'libs/testlib/stencil.config.ts',
+                  },
+                },
+                e2e: {
+                  builder: '@nxext/stencil:e2e',
+                  options: {
+                    projectType: 'library',
+                    configPath: 'libs/testlib/stencil.config.ts',
+                  },
+                },
+                serve: {
+                  builder: '@nxext/stencil:serve',
+                  options: {
+                    projectType: 'library',
+                    configPath: 'libs/testlib/stencil.config.ts',
+                  },
+                },
+              },
+            },
+          },
+          cli: {
+            defaultCollection: '@nxext/stencil',
+          },
+          schematics: {
+            '@nrwl/workspace': {
+              library: {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/cypress': {
+              'cypress-project': {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/react': {
+              application: {
+                linter: 'eslint',
+              },
+              library: {
+                linter: 'eslint',
+              },
+              'storybook-configuration': {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/next': {
+              application: {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/web': {
+              application: {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/node': {
+              application: {
+                linter: 'eslint',
+              },
+              library: {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/nx-plugin': {
+              plugin: {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/nest': {
+              application: {
+                linter: 'eslint',
+              },
+            },
+            '@nrwl/express': {
+              application: {
+                linter: 'eslint',
+              },
+              library: {
+                linter: 'eslint',
+              },
+            },
+          },
+        })
+      );
 
       initialTree = await schematicRunner
         .runSchematicAsync('update-0-1-0', {}, initialTree)
