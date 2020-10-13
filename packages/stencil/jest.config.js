a6b93fc081c3d4ecf5fa0d5400f84cce9b8389ae
Dominik Pieper <dominik@pieper.io>
2020-10-13 08:44:38 +0200

    chore(nxext): update nx to 10.3

--- packages/stencil/jest.config.js
+++ packages/stencil/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'stencil',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/packages/stencil',
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'stencil',
 };
