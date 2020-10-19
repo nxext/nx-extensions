4f0fb8bfacb38ec1e285d31e37472b8daa11e3a5
Dominik Pieper <dominik@pieper.io>
2020-10-19 20:31:38 +0200

    feat(tailwind): add svelte tailwind schematic

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   projects: [
     '<rootDir>/packages/stencil',
     '<rootDir>/packages/svelte',
-    '<rootDir>/packages/stencil-core-utils',
     '<rootDir>/packages/tailwind',
     '<rootDir>/e2e/tailwind-e2e',
   ],
