b8ab7a0304315b8523ba9fb257fb5faa4dd67555
Dominik Pieper <dominik@pieper.io>
2020-10-20 16:45:42 +0200

    chore(stencil): update version to 2.1.0

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,17 +1,17 @@
-export const nxextVersion = '10.1.0';
-export const stencilVersion = '^2.0.0';
+export const nxextVersion = '10.3.0';
+export const stencilVersion = '^2.1.0';
 export const stencilRouterVersion = '^1.0.1';
-export const ionicVersion = '^5.3.0';
+export const ionicVersion = '^5.4.0';
 
 export const autoprefixerVersion = '^9.7.6';
 export const autoprefixerTypesVersion = '^9.7.2';
 
-export const puppeteer = '^2.1.1';
-export const puppeteerType = '^2.0.1';
+export const puppeteer = '~5.3.1';
+export const puppeteerType = '~3.0.2';
 
-export const storybookVersion = '5.3.9';
+export const storybookVersion = '^6.0.21';
 
-export const nxVersion = '^10.0.0';
+export const nxVersion = '^10.3.0';
 
 export const reactOutputTargetVersion = '^0.0.9';
 export const angularOutputTargetVersion = '^0.0.5';
