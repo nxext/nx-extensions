aff199c062b434b78c89b8dfec603870b434d776
Danny McGee <dmcgee@insiten.com>
2020-11-03 13:03:29 -0500

    Enable error reporting

--- packages/stencil/src/builders/test/builder.ts
+++ packages/stencil/src/builders/test/builder.ts
@@ -33,7 +33,7 @@ export function runBuilder(
     options,
     context,
     createStencilCompilerOptions
-  ).pipe(createStencilProcess());
+  ).pipe(createStencilProcess(context));
 }
 
 export default createBuilder(runBuilder);
