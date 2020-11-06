e6a1b0eeca148a5fafdbe0cc15da82bda30248ff
Dominik Pieper <dominik@pieper.io>
2020-11-06 10:07:30 +0100

    chore(stencil): update stencil versions

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxextVersion = '10.3.0';
-export const stencilVersion = '^2.2.0';
+export const stencilVersion = '^2.3.0';
 export const stencilRouterVersion = '^1.0.1';
-export const ionicVersion = '^5.4.0';
+export const ionicVersion = '^5.4.2';
 
 export const autoprefixerVersion = '^9.7.6';
 export const autoprefixerTypesVersion = '^9.7.2';
@@ -10,18 +10,21 @@ export const puppeteer = '~5.3.1';
 export const puppeteerType = '~3.0.2';
 
 export const storybookVersion = '^6.0.21';
-
 export const nxVersion = '^10.3.0';
 
-export const reactOutputTargetVersion = '^0.0.9';
-export const angularOutputTargetVersion = '^0.0.5';
-export const vueOutputTargetVersion = '^0.1.8';
+export const STENCIL_OUTPUTTARGET_VERSION: {
+  [style: string]: string;
+} = {
+  react: '^0.0.9',
+  angular: '^0.0.5',
+  pcss: '^0.1.8',
+};
 
 export const STENCIL_STYLE_PLUGIN_VERSION: {
   [style: string]: string;
 } = {
   less: '1.0.0',
-  scss: '1.3.1',
+  scss: '1.3.2',
   pcss: '1.0.1',
-  styl: '1.0.0',
+  styl: '1.0.1',
 };
