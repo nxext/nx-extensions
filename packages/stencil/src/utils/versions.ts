63b35c56f6941482fff137fe0aa9337be6ceab88
Dominik Pieper <dominik@pieper.io>
2020-11-20 10:46:09 +0100

    test(stencil): reenable tmp disabled e2e tests

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxextVersion = '10.3.0';
 export const stencilVersion = '^2.3.0';
 export const stencilRouterVersion = '^1.0.1';
-export const ionicVersion = '^5.4.2';
+export const ionicVersion = '^5.5.0';
 
 export const autoprefixerVersion = '^9.7.6';
 export const autoprefixerTypesVersion = '^9.7.2';
@@ -12,7 +12,7 @@ export const puppeteer = '~5.3.1';
 export const puppeteerType = '~3.0.2';
 
 export const storybookVersion = '^6.0.21';
-export const nxVersion = '^10.3.0';
+export const nxVersion = '^10.4.0';
 
 export const STENCIL_OUTPUTTARGET_VERSION: {
   [style: string]: string;
