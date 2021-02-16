1c9480263e1fc2618ef40d7bcb7c0cefb607c1f2
Dominik Pieper <dominik@pieper.io>
2021-02-16 13:40:29 +0100

    feat(svelte): add generated component to barrel in library

---
+++ packages/svelte/src/schematics/component/lib/create-component-in-project.ts
@@ -0,0 +1,23 @@
+import { applyTemplates, chain, move, Rule, SchematicContext, Tree, url } from '@angular-devkit/schematics';
+import { getProjectConfig } from '@nrwl/workspace';
+import { join, normalize } from '@angular-devkit/core';
+import { applyWithSkipExisting } from '../../utils/utils';
+import { names } from '@nrwl/devkit';
+import { ComponentSchema } from '../component';
+
+export function createComponentInProject(options: ComponentSchema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const projectConfig = getProjectConfig(tree, options.project);
+
+    const projectDirectory = options.directory
+      ? join(normalize(options.directory))
+      : join(normalize(''));
+
+    return chain([
+      applyWithSkipExisting(url('./files/src'), [
+        applyTemplates(names(options.name)),
+        move(join(normalize(projectConfig.sourceRoot), projectDirectory)),
+      ]),
+    ])(tree, context);
+  };
+}
