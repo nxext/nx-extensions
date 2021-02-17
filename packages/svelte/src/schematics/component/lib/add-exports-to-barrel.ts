e021cea2e62b14452aac505092caee0343c77c7c
Dominik Pieper <dominik@pieper.io>
2021-02-17 07:42:14 +0100

    refactor(svelte): refactor svelte schematics

--- packages/svelte/src/schematics/component/lib/add-exports-to-barrel.ts
+++ packages/svelte/src/schematics/component/lib/add-exports-to-barrel.ts
@@ -1,23 +1,18 @@
 import * as ts from 'typescript';
-import * as path from 'path';
 import { Rule, Tree } from '@angular-devkit/schematics';
 
 import { insert, addGlobal, getProjectConfig, ProjectType } from '@nrwl/workspace';
 import { ComponentSchema } from '../component';
 import { names } from '@nrwl/devkit';
+import { join } from 'path';
 
 export function addExportsToBarrel(options: ComponentSchema): Rule {
   return (tree: Tree) => {
     const projectConfig = getProjectConfig(tree, options.project);
 
-    const name = names(options.name);
-    const projectDirectory = options.directory
-      ? path.join(path.normalize(options.directory))
-      : path.join(path.normalize(''));
-    const creationDir = path.join(path.normalize(projectConfig.sourceRoot), 'components', projectDirectory);
-    const componentFile = path.join(creationDir, name.fileName, `${name.className}.svelte`);
-    const indexFilePath = path.join(projectConfig.sourceRoot, 'index.ts');
-    const relativeComponentFilePath = path.relative(projectConfig.sourceRoot, componentFile);
+    const { className, fileName } = names(options.name);
+    const indexFilePath = join(projectConfig.sourceRoot, 'index.ts');
+    const componentFile = `./components/${fileName}/${className}.svelte`;
 
     if(projectConfig.projectType === ProjectType.Library) {
       const buffer = tree.read(indexFilePath);
@@ -35,7 +30,7 @@ export function addExportsToBarrel(options: ComponentSchema): Rule {
           ...addGlobal(
             indexSourceFile,
             indexFilePath,
-            `export { default as default } from './${relativeComponentFilePath}';`
+            `export { default as default } from '${componentFile}';`
           )
         ]);
       }
