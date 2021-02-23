07a287f50d2d951c3a5b254167fc2b9a7dac043b
Dominik Pieper <dominik@pieper.io>
2021-02-23 22:38:48 +0100

    refactor(tailwind): enhance e2e test

--- e2e/tailwind-e2e/tests/tailwind.test.ts
+++ e2e/tailwind-e2e/tests/tailwind.test.ts
@@ -1,4 +1,4 @@
-import { runNxCommandAsync, uniq } from '@nrwl/nx-plugin/testing';
+import { runNxCommandAsync, uniq, updateFile } from '@nrwl/nx-plugin/testing';
 import { ensureNxProjectWithDeps } from '../utils/testing';
 
 describe('tailwind e2e', () => {
@@ -11,6 +11,12 @@ describe('tailwind e2e', () => {
 
     await runNxCommandAsync(`generate @nxext/stencil:app ${plugin}`);
     await runNxCommandAsync(`generate @nxext/tailwind:stencil ${plugin}`);
+
+    updateFile(`apps/${plugin}/src/global/app.css`, `
+          @tailwind base;
+          @tailwind components;
+          @tailwind utilities;
+    `);
 
     const result = await runNxCommandAsync(`build ${plugin}`);
     expect(result.stdout).toContain('build finished');
