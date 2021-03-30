28af1000fb3aae847d6ed42d547968ff6d744daa
Dominik Pieper <dominik@pieper.io>
2021-03-30 19:43:18 +0100

    chore(nxext): update dependencies

--- packages/svelte/src/generators/init/lib/add-dependencies.ts
+++ packages/svelte/src/generators/init/lib/add-dependencies.ts
@@ -4,9 +4,9 @@ import { Schema } from '../schema';
 export function updateDependencies(tree: Tree, schema: Schema) {
   let devDependencies = {
     'svelte-jester': '^1.3.0',
-    svelte: '^3.35.0',
+    svelte: '^3.36.0',
     'svelte-check': '^1.2.3',
-    'svelte-preprocess': '^4.6.9',
+    'svelte-preprocess': '^4.7.0',
     '@tsconfig/svelte': '^1.0.10',
     '@testing-library/svelte': '^3.0.3',
     'rollup-plugin-local-resolve': '^1.0.7',
