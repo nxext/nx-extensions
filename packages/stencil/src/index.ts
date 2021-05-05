d2fbfbbac21ecdc78cc5817152e99e0b4fdcfdbe
Dominik Pieper <dominik@pieper.io>
2021-05-05 19:52:53 +0200

    refactor(tailwind): migrate stencil to nx generator

---
+++ packages/stencil/src/index.ts
@@ -0,0 +1,7 @@
+export { initGenerator } from './schematics/init/init';
+export { applicationGenerator } from './schematics/application/schematic';
+export { outputtargetGenerator } from './schematics/add-outputtarget/add-outputtarget';
+export { libraryGenerator } from './schematics/library/schematic';
+export { componentGenerator } from './schematics/component/component';
+export { ionicPwaGenerator } from './schematics/ionic-pwa/schematic';
+export { makeLibBuildableGenerator } from './schematics/make-lib-buildable/make-lib-buildable';
--- packages/tailwind/src/schematics/stencil/schematic.ts
+++ packages/tailwind/src/schematics/stencil/schematic.ts
@@ -1,148 +1,60 @@
 import {
-  applyTemplates,
-  chain,
-  move,
-  Rule,
-  SchematicContext,
-  Tree,
-  url,
-} from '@angular-devkit/schematics';
-import {
-  addDepsToPackageJson,
-  formatFiles,
-  getProjectConfig,
-  insert,
-} from '@nrwl/workspace';
-import { applyWithSkipExisting } from '../../utils/utils';
-import * as ts from 'typescript';
-import { Change, insertImport } from '@nrwl/workspace/src/utils/ast-utils';
-import { readTsSourceFileFromTree } from '../../utils/ast-utils';
-import { addCodeIntoArray } from '../../utils/utils';
-import { stripIndents } from '@angular-devkit/core/src/utils/literals';
-import { wrapAngularDevkitSchematic } from '@nrwl/devkit/ngcli-adapter';
-import { logger } from '@nrwl/devkit';
+  addDependenciesToPackageJson,
+  convertNxGenerator,
+  formatFiles, generateFiles, joinPathFragments,
+  logger, readProjectConfiguration,
+  stripIndents,
+  Tree
+} from '@nrwl/devkit';
+import { addStylePluginToConfigInTree } from './lib/ast-utils';
 
 export interface Schema {
   project: string;
   skipFormat: boolean;
 }
 
-export function addToPlugins(
-  source: ts.SourceFile,
-  file: string,
-  toInsert: string
-): Change[] {
-  const pluginsIdentifier = 'plugins';
-  return addCodeIntoArray(source, pluginsIdentifier, toInsert, file);
+export function createTailwindConfigForProject(tree: Tree, options: Schema) {
+  const projectConfig = readProjectConfiguration(tree, options.project);
+  const sourceRoot = projectConfig.sourceRoot;
+  const projectRoot = projectConfig.root;
+
+  generateFiles(tree, joinPathFragments(__dirname, './files'), projectRoot, {
+    sourceRoot,
+    projectRoot
+  });
 }
 
-function createTailwindConfigForProject(options: Schema): Rule {
-  return (tree: Tree, context: SchematicContext) => {
-    const projectConfig = getProjectConfig(tree, options.project);
-    const sourceRoot = projectConfig.sourceRoot;
-    const projectRoot = projectConfig.root;
 
-    return chain([
-      applyWithSkipExisting(url('./files'), [
-        applyTemplates({
-          sourceRoot,
-        }),
-        move(projectRoot),
-      ]),
-    ])(tree, context);
-  };
+async function tailwindStencilGenerator(tree: Tree, options: Schema) {
+  createTailwindConfigForProject(tree, options);
+  addStylePluginToConfigInTree(tree, options);
+
+  if (!options.skipFormat) {
+    await formatFiles(tree);
+  }
+
+  logger.info(stripIndents`
+    To use Tailwindcss, import the modules you need into your app.css (or scss, less, styl or whatever your global file extension is).
+
+    Import e.g.:
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+
+    More informations about tailwindcss and tailwind imports: https://tailwindcss.com/docs/installation#include-tailwind-in-your-css
+  `);
+
+  return addDependenciesToPackageJson(
+    tree,
+    {},
+    {
+      '@stencil/postcss': '^2.0.0',
+      tailwindcss: 'npm:@tailwindcss/postcss7-compat',
+      autoprefixer: '^9.0.0',
+      cssnano: '^4.1.10'
+    }
+  );
 }
 
-export function addStylePluginToConfigInTree(options: Schema): Rule {
-  return (tree: Tree) => {
-    const projectConfig = getProjectConfig(tree, options.project);
-    const projectRoot = projectConfig.root;
-    const stencilConfigPath =
-      projectConfig?.architect?.build?.options?.configPath ||
-      projectConfig?.architect?.test?.options?.configPath;
-    const stencilConfigSource: ts.SourceFile = readTsSourceFileFromTree(
-      tree,
-      stencilConfigPath
-    );
-
-    insert(tree, stencilConfigPath, [
-      insertImport(
-        stencilConfigSource,
-        stencilConfigPath,
-        'postcss',
-        '@stencil/postcss'
-      ),
-      insertImport(
-        stencilConfigSource,
-        stencilConfigPath,
-        'autoprefixer',
-        'autoprefixer',
-        true
-      ),
-      insertImport(
-        stencilConfigSource,
-        stencilConfigPath,
-        'cssnano',
-        'cssnano',
-        true
-      ),
-      insertImport(
-        stencilConfigSource,
-        stencilConfigPath,
-        'tailwindcss',
-        'tailwindcss',
-        true
-      ),
-      ...addToPlugins(
-        stencilConfigSource,
-        stencilConfigPath,
-        `
-          postcss({
-            plugins: [
-              tailwindcss('./${projectRoot}/tailwind.config.js'),
-              autoprefixer(),
-              cssnano()
-            ]
-          })
-          `
-      ),
-    ]);
-
-    return tree;
-  };
-}
-
-export function tailwindStencilSchematic(options: Schema): Rule {
-  return chain([
-    addDepsToPackageJson(
-      {},
-      {
-        '@stencil/postcss': '^2.0.0',
-        tailwindcss: 'npm:@tailwindcss/postcss7-compat',
-        autoprefixer: '^9.0.0',
-        cssnano: '^4.1.10',
-      }
-    ),
-    createTailwindConfigForProject(options),
-    addStylePluginToConfigInTree(options),
-    () => {
-      logger.info(stripIndents`
-          To use Tailwindcss, import the modules you need into your app.css (or scss, less, styl or whatever your global file extension is).
-
-          Import e.g.:
-          @tailwind base;
-          @tailwind components;
-          @tailwind utilities;
-
-          More informations about tailwindcss and tailwind imports: https://tailwindcss.com/docs/installation#include-tailwind-in-your-css
-      `);
-    },
-    formatFiles({ skipFormat: options.skipFormat }),
-  ]);
-}
-
-export default tailwindStencilSchematic;
-export const tailwindStencilGenerator = wrapAngularDevkitSchematic(
-  '@nxext/tailwind',
-  'stencil'
-);
+export default tailwindStencilGenerator;
+export const tailwindStencilSchematic = convertNxGenerator(tailwindStencilGenerator);
