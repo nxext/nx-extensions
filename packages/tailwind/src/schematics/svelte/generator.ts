0877f6d3be3ec4861ec8955a793a6d66ae64ae09
Dominik Pieper <dominik@pieper.io>
2021-05-09 12:26:32 +0200

    chore(nxext): update dependencies

--- packages/tailwind/src/schematics/svelte/generator.ts
+++ packages/tailwind/src/schematics/svelte/generator.ts
@@ -29,7 +29,7 @@ async function tailwindSvelteGenerator(tree: Tree, options: Schema) {
     tree,
     {},
     {
-      tailwindcss: '^2.1.1',
+      tailwindcss: '^2.1.2',
       'postcss-import': '^14.0.0',
       'postcss-nested': '^5.0.0',
     }
