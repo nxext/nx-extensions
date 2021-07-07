107e9a7604d1aa958adf57744d28e1bdd56684a6
Dominik Pieper <dominik@pieper.io>
2021-07-07 19:41:29 +0200

    feat(stencil): add migration to add the outputPath

--- packages/stencil/src/migrations/update-12-0-7/update-12-0-7.spec.ts
+++ packages/stencil/src/migrations/update-12-0-7/update-12-0-7.spec.ts
@@ -12,16 +12,51 @@ describe('update-12-0-7', () => {
   it(`should update generator definition`, async () => {
     updateJson(tree, 'workspace.json', workspaceJson => {
       workspaceJson.projects.app = {
+        "root": "apps/app",
         "targets": {
           "build": {
-            "executor": "@nxext/stencil:build"
+            "builder": "@nxext/stencil:build",
+            "outputs": ["{options.outputPath}"],
+            "options": {
+              "projectType": "application",
+              "tsConfig": "apps/app/tsconfig.app.json",
+              "configPath": "apps/app/stencil.config.ts"
+            },
+            "configurations": {
+              "production": {
+                "dev": false
+              }
+            }
+          },
+          "serve": {
+            "builder": "@nxext/stencil:build",
+            "outputs": ["{options.outputPath}"],
+            "options": {
+              "projectType": "application",
+              "tsConfig": "apps/app/tsconfig.app.json",
+              "configPath": "apps/app/stencil.config.ts",
+              "serve": true,
+              "watch": true
+            }
+          },
+          "test": {
+            "builder": "@nxext/stencil:test",
+            "outputs": ["{options.outputPath}"],
+            "options": {
+              "projectType": "application",
+              "tsConfig": "apps/app/tsconfig.app.json",
+              "configPath": "apps/app/stencil.config.ts"
+            }
+          },
+          "e2e": {
+            "builder": "@nxext/stencil:e2e",
+            "outputs": ["{options.outputPath}"],
+            "options": {
+              "projectType": "application",
+              "tsConfig": "apps/app/tsconfig.app.json",
+              "configPath": "apps/app/stencil.config.ts"
+            }
           }
-        },
-        "generators": {
-          "@nxext/stencil:component": {
-            "style": "css",
-            "storybook": "false",
-          },
         }
       }
 
@@ -31,31 +66,13 @@ describe('update-12-0-7', () => {
     await update(tree);
     const workspaceJson = readJson(tree, 'workspace.json');
 
-    expect(workspaceJson.projects.app.generators['@nxext/stencil:component'])
-      .toEqual({
-        style: 'css'
-      });
-  });
-
-  it(`should ignore generator definitionwithout storybook set`, async () => {
-    updateJson(tree, 'workspace.json', workspaceJson => {
-      workspaceJson.projects.app = {
-        "generators": {
-          "@nxext/stencil:component": {
-            "style": "css"
-          },
-        }
-      }
-
-      return workspaceJson;
-    });
-
-    await update(tree);
-    const workspaceJson = readJson(tree, 'workspace.json');
-
-    expect(workspaceJson.projects.app.generators['@nxext/stencil:component'])
-      .toEqual({
-        style: 'css'
-      });
+    expect(workspaceJson.projects.app.targets.build.options?.outputPath)
+      .toEqual("dist/apps/app");
+    expect(workspaceJson.projects.app.targets.test.options?.outputPath)
+      .toEqual("dist/apps/app");
+    expect(workspaceJson.projects.app.targets.e2e.options?.outputPath)
+      .toEqual("dist/apps/app");
+    expect(workspaceJson.projects.app.targets.serve.options?.outputPath)
+      .toEqual("dist/apps/app");
   });
 });
