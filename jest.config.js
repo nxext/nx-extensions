8bc8a3af8fe122134514c4770cc8e4dc92db3d22
Dominik Pieper <dominik@pieper.io>
2021-07-23 06:26:46 +0200

    chore(nxext): update Nx to 12.6

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
-    '<rootDir>/packages/stencil',
-    '<rootDir>/packages/svelte',
+    ...getJestProjects(),
     '<rootDir>/packages/tailwind',
     '<rootDir>/e2e/tailwind-e2e',
-    '<rootDir>/packages/sveltekit',
     '<rootDir>/e2e/sveltekit-e2e',
-    '<rootDir>/packages/vite',
     '<rootDir>/e2e/vite-e2e',
   ],
 };
