dc2daad474e8f7c02e93e1dd4489df189507b861
Dominik Pieper <dominik@pieper.io>
2021-09-02 06:22:17 +0200

    chore(stencil): update deps

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const stencilVersion = '2.6.0';
 export const stencilRouterVersion = '^1.0.1';
-export const ionicVersion = '^5.6.11';
+export const ionicVersion = '^5.6.14';
 
 export const autoprefixerVersion = '^9.7.6';
 export const autoprefixerTypesVersion = '^9.7.2';
@@ -14,9 +14,9 @@ export const eslintPluginStencilVersion = '0.3.1';
 export const STENCIL_OUTPUTTARGET_VERSION: {
   [style: string]: string;
 } = {
-  react: '^0.0.9',
-  angular: '^0.0.6',
-  vue: '^0.5.0',
+  react: '^0.0.12',
+  angular: '^0.0.7',
+  vue: '^0.5.1',
   svelte: '^0.0.3',
 };
 
