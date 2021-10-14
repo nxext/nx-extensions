c52ba648bc9673f30ae32cffe69a2c5edbb9a733
Dominik Pieper <dominik@pieper.io>
2021-10-14 06:53:34 +0200

    chore(stencil): update Stencil to 2.9 which will work again

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,12 +1,12 @@
-export const stencilVersion = '2.6.0';
+export const stencilVersion = '2.9.0';
 export const stencilRouterVersion = '^1.0.1';
-export const ionicVersion = '^5.6.14';
+export const ionicVersion = '^5.8.4';
 
 export const autoprefixerVersion = '^9.7.6';
 export const autoprefixerTypesVersion = '^9.7.2';
 
 export const puppeteer = '~5.3.1';
-export const puppeteerType = '~3.0.2';
+export const puppeteerType = '~5.4.4';
 
 export const eslintPluginImportVersion = '2.22.1';
 export const eslintPluginStencilVersion = '0.3.1';
@@ -14,8 +14,8 @@ export const eslintPluginStencilVersion = '0.3.1';
 export const STENCIL_OUTPUTTARGET_VERSION: {
   [style: string]: string;
 } = {
-  react: '^0.0.12',
-  angular: '^0.0.7',
+  react: '^0.1.0',
+  angular: '^0.2.0',
   vue: '^0.5.1',
   svelte: '^0.0.3',
 };
@@ -24,7 +24,7 @@ export const STENCIL_STYLE_PLUGIN_VERSION: {
   [style: string]: string;
 } = {
   less: '1.0.0',
-  scss: '1.4.1',
-  pcss: '2.0.0',
+  scss: '1.5.2',
+  pcss: '2.1.0',
   styl: '1.0.1',
 };
