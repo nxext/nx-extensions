4da96d19e828e80b1d865b16763977dc73d9142e
Dominik Pieper <dominik@pieper.io>
2021-11-09 16:10:31 +0100

    fix(stencil): set jest-config version for jest 26

--- packages/stencil/src/generators/init/init.ts
+++ packages/stencil/src/generators/init/init.ts
@@ -29,6 +29,7 @@ export async function initGenerator<T extends InitSchema>(
   updateJson(host, 'package.json', pkgJson => {
     pkgJson.devDependencies['jest'] = '26.6.3';
     pkgJson.devDependencies['ts-jest'] = '26.5.6';
+    pkgJson.devDependencies['jest-config'] = '26.6.3';
 
     return pkgJson;
   })
