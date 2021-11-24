190ba2c504ca65b0a9cbba1644834ecca5cd85fe
Kristian Mandrup <kmandrup@gmail.com>
2021-11-24 14:56:32 +0700

    add solidjs executors for vite and remove rollup dependencies

---
+++ packages/solid/src/executors/dev/executor.ts
@@ -0,0 +1,39 @@
+import { DevExecutorSchema } from './schema';
+import { ExecutorContext, joinPathFragments } from '@nrwl/devkit';
+import { createServer } from 'vite';
+import { relative } from 'path';
+import { RollupWatcherEvent } from 'rollup';
+
+export default async function runExecutor(
+  options: DevExecutorSchema,
+  context: ExecutorContext
+) {
+  const projectDir = context.workspace.projects[context.projectName].root;
+  const projectRoot = joinPathFragments(`${context.root}/${projectDir}`);
+
+  const server = await createServer({
+    root: projectRoot,
+    build: {
+      outDir: relative(
+        projectRoot,
+        joinPathFragments(`${context.root}/dist/${projectDir}`)
+      ),
+      emptyOutDir: true,
+    },
+  });
+
+  const devProcess = await server.listen();
+  await new Promise<void>((resolve, reject) => {
+    devProcess.watcher.on('event', (data: RollupWatcherEvent) => {
+      if (data.code === 'END') {
+        resolve();
+      } else if (data.code === 'ERROR') {
+        reject();
+      }
+    });
+  });
+
+  await devProcess.close();
+
+  return { success: true };
+}
