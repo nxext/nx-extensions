fd7cd90d9ae6f9f9cb860e33ef050c54ac2740b6
Kristian Mandrup <kmandrup@gmail.com>
2021-11-29 14:04:12 +0700

    fix generator files and add vite to solid dependencies

--- packages/solid/src/generators/init/lib/add-dependencies.ts
+++ packages/solid/src/generators/init/lib/add-dependencies.ts
@@ -5,6 +5,7 @@ export function updateDependencies(tree: Tree) {
     'solid-jest': '^0.2.0',
     'solid-js': '^1.2.5',
     'solid-testing-library': '^0.2.1',
+    vite: '^2.5.7',
     'vite-plugin-solid': '2.1.2',
     'eslint-plugin-solid': '0.1.2',
   };
