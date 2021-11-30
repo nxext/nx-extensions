2db09b4c3f05cd4b9a3c6f8cafe5ca0cb993fcff
Dominik Pieper <dominik@pieper.io>
2021-11-30 17:48:01 +0100

    chore(nxext): update deps

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,4 +1,4 @@
-export const stencilVersion = '^2.10.0';
+export const stencilVersion = '^2.11.0';
 export const stencilRouterVersion = '^1.0.1';
 export const ionicVersion = '^5.8.4';
 
@@ -15,7 +15,7 @@ export const STENCIL_OUTPUTTARGET_VERSION: {
   [style: string]: string;
 } = {
   react: '^0.1.0',
-  angular: '^0.2.0',
+  angular: '^0.3.0',
   vue: '^0.5.1',
   svelte: '^0.0.3',
 };
