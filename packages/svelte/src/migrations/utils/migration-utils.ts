fabbe841c6ffc905dc297be8ed3b4718ac2e781c
Dominik Pieper <dominik@pieper.io>
2021-12-14 17:25:27 +0100

    fix(svelte): add svelte migration to vite

---
+++ packages/svelte/src/migrations/utils/migration-utils.ts
@@ -0,0 +1,28 @@
+import { getProjects, ProjectConfiguration, Tree } from '@nrwl/devkit';
+
+export function getSvelteLegacyTargetProjects(tree: Tree) {
+  const projects = getProjects(tree);
+  const filtered = new Map<string, ProjectConfiguration>();
+  projects.forEach((value: ProjectConfiguration, key: string) => {
+    if(isSvelteLegacyTargetProject(value)) {
+      filtered.set(key, value);
+    }
+  })
+
+  return filtered;
+}
+
+export function isSvelteLegacyTargetProject(
+  project: any
+): boolean {
+  return ['build', 'test', 'serve', 'e2e'].map(command => {
+    const target =
+      project.targets && project.targets[command]
+        ? project.targets[command]
+        : {};
+    return (
+      target &&
+      target.executor === `@nxext/svelte:${command}`
+    );
+  }).some(value => value === true);
+}
