257d0885895bf6141429536ab2431f6e07f9a3ba
Jordan Hall <jordan@libertyware.co.uk>
2021-12-18 19:13:29 +0000

    fix: repair build

--- packages/angular/src/generators/application/application.ts
+++ packages/angular/src/generators/application/application.ts
@@ -36,23 +36,7 @@ export async function applicationGenerator(tree: Tree, options: Schema) {
   tree.delete(joinPathFragments(appProjectRoot, 'tsconfig.spec.json'));
   tree.delete(joinPathFragments(appProjectRoot, 'tsconfig.json'));
   tree.delete(joinPathFragments(appProjectRoot, 'src', 'index.html'));
-  tree.delete(
-    joinPathFragments(
-      appProjectRoot,
-      'src',
-      'app',
-      `nx-welcome.${options.js ? 'jsx' : 'tsx'}`
-    )
-  );
-  const fileName = options.pascalCaseFiles ? 'App' : 'app';
-  tree.delete(
-    joinPathFragments(
-      appProjectRoot,
-      'src',
-      'app',
-      `${fileName}.${options.js ? 'jsx' : 'tsx'}`
-    )
-  );
+
   const projectConfig = readProjectConfiguration(tree, appProjectName);
   updateProjectConfiguration(tree, appProjectName, {
     ...projectConfig,
@@ -88,7 +72,6 @@ export async function applicationGenerator(tree: Tree, options: Schema) {
     tmpl: '',
     offsetFromRoot: offsetFromRoot(appProjectRoot),
     projectName: appProjectName,
-    fileName: fileName,
   };
 
   generateFiles(
