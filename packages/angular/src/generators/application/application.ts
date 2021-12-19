13a39c29c515936b53ba5818eb0aa224fea8c90c
Jordan Hall <jordan@libertyware.co.uk>
2021-12-19 04:07:37 +0000

    fix: first attempt at adding missing generator imports

--- packages/angular/src/generators/application/application.ts
+++ packages/angular/src/generators/application/application.ts
@@ -10,10 +10,33 @@ import {
   updateProjectConfiguration,
   readProjectConfiguration,
   offsetFromRoot,
+  StringChange,
+  ChangeType,
+  applyChangesToString,
 } from '@nrwl/devkit';
 import { Schema } from './schema';
 import { applicationGenerator as nxApplicationGenerator } from '@nrwl/angular/generators';
 import { E2eTestRunner } from '@nrwl/angular/src/utils/test-runners';
+import {
+  createSourceFile,
+  ScriptTarget,
+  SourceFile,
+  SyntaxKind,
+} from 'typescript';
+import { findNodes } from '@nrwl/workspace';
+
+export function addImport(
+  source: SourceFile,
+  statement: string
+): StringChange[] {
+  return [
+    {
+      type: ChangeType.Insert,
+      index: 0,
+      text: `\n${statement}\n`,
+    },
+  ];
+}
 
 export async function applicationGenerator(tree: Tree, options: Schema) {
   const appDirectory = options.directory
@@ -36,6 +59,20 @@ export async function applicationGenerator(tree: Tree, options: Schema) {
   tree.delete(joinPathFragments(appProjectRoot, 'tsconfig.spec.json'));
   tree.delete(joinPathFragments(appProjectRoot, 'tsconfig.json'));
   tree.delete(joinPathFragments(appProjectRoot, 'src', 'index.html'));
+  const mainTsFilePath = joinPathFragments(appProjectRoot, 'src', 'main.ts');
+  const source = tree.read(mainTsFilePath, 'utf-8');
+  const mainTsSourceFile = createSourceFile(
+    joinPathFragments(appProjectRoot, 'src', 'main.ts'),
+    source,
+    ScriptTarget.Latest,
+    true
+  );
+  const changes = applyChangesToString(source, [
+    ...addImport(mainTsSourceFile, `import '@angular/compiler';`),
+    ...addImport(mainTsSourceFile, `zone.js';`),
+  ]);
+
+  tree.write(mainTsFilePath, changes);
 
   const projectConfig = readProjectConfiguration(tree, appProjectName);
   updateProjectConfiguration(tree, appProjectName, {
