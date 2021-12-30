6f504f59b6048a6065b47dbbdb64ba19d4a1aa8f
Dominik Pieper <dominik@pieper.io>
2021-12-30 14:27:40 +0100

    refactor(solid): use vite deps from viteInitGenerator and remove own vite package install

--- packages/solid/src/generators/init/init.ts
+++ packages/solid/src/generators/init/init.ts
@@ -10,6 +10,7 @@ import { addJestPlugin } from './lib/add-jest-plugin';
 import { addCypressPlugin } from './lib/add-cypress-plugin';
 import { updateDependencies } from './lib/add-dependencies';
 import { addLinterPlugin } from './lib/add-linter-plugin';
+import { viteInitGenerator } from '@nxext/vite';
 
 export async function initGenerator(host: Tree, schema: Schema) {
   const tasks: GeneratorCallback[] = [];
@@ -27,6 +28,8 @@ export async function initGenerator(host: Tree, schema: Schema) {
   tasks.push(linterTask);
   const installTask = updateDependencies(host);
   tasks.push(installTask);
+  const viteTask = await viteInitGenerator(host, schema);
+  tasks.push(viteTask);
 
   if (!schema.skipFormat) {
     await formatFiles(host);
