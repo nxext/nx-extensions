9a7b4980f2a2a7c1c42742e92bef9f97d70356e8
Dominik Pieper <dominik@pieper.io>
2022-01-01 12:36:08 +0100

    chore(nxext): fix depdendencies for Nx 13.4

--- packages/preact/src/generators/init/lib/add-dependencies.ts
+++ packages/preact/src/generators/init/lib/add-dependencies.ts
@@ -1,15 +1,20 @@
 import { addDependenciesToPackageJson, Tree } from '@nrwl/devkit';
-
-export const preactVersion = '^10.6.2';
-export const testingLibraryPreactVersion = '^2.0.1';
-export const vitePluginPreactVersion = '^2.1.5';
-export const eslintPluginPreactVersion = '^0.1.0';
+import {
+  eslintPluginPreactVersion,
+  preactVersion,
+  testingLibraryPreactVersion,
+  vitePluginPreactVersion,
+} from '../../utils/versions';
 
 export function updateDependencies(tree: Tree) {
-  return addDependenciesToPackageJson(tree, {}, {
-    preact: preactVersion,
-    '@testing-library/preact': testingLibraryPreactVersion,
-    '@preact/preset-vite': vitePluginPreactVersion,
-    'eslint-plugin-preact': eslintPluginPreactVersion,
-  });
+  return addDependenciesToPackageJson(
+    tree,
+    {},
+    {
+      preact: preactVersion,
+      '@testing-library/preact': testingLibraryPreactVersion,
+      '@preact/preset-vite': vitePluginPreactVersion,
+      'eslint-plugin-preact': eslintPluginPreactVersion,
+    }
+  );
 }
