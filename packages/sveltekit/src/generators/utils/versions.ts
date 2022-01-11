3c5d3ad23b92b18b949d8c0ae45870de320d9295
Dominik Pieper <dominik@pieper.io>
2022-01-11 05:35:53 +0100

    chore(sveltekit): update sveltekit versions

--- packages/sveltekit/src/generators/utils/versions.ts
+++ packages/sveltekit/src/generators/utils/versions.ts
@@ -1,6 +1,5 @@
 export const eslintPluginSvelteVersion = '^3.1.2';
-export const svelteVersion = '^3.35.0';
+export const svelteVersion = '^3.44.0';
 export const svelteKitVersion = 'next';
 export const svelteKitAdapterVersion = 'next';
-export const sveltePreprocessVersion = '^4.0.0';
-export const viteVersion = '^2.1.0';
+export const sveltePreprocessVersion = '^4.9.4';
