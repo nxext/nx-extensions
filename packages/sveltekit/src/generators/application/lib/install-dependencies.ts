6593519ed4fe89a5e9196b827a31e17fe217cc42
Dominik Pieper <dominik@pieper.io>
2022-01-11 05:35:53 +0100

    chore(sveltekit): update sveltekit versions

--- packages/sveltekit/src/generators/application/lib/install-dependencies.ts
+++ packages/sveltekit/src/generators/application/lib/install-dependencies.ts
@@ -4,15 +4,17 @@ import {
   svelteKitVersion,
   sveltePreprocessVersion,
   svelteVersion,
-  viteVersion
 } from '../../utils/versions';
 
 export function installDependencies(host: Tree) {
-  return addDependenciesToPackageJson(host, {}, {
-    '@sveltejs/adapter-node': svelteKitAdapterVersion,
-    '@sveltejs/kit': svelteKitVersion,
-    'svelte': svelteVersion,
-    'vite': viteVersion,
-    'svelte-preprocess': sveltePreprocessVersion
-  });
+  return addDependenciesToPackageJson(
+    host,
+    {},
+    {
+      '@sveltejs/adapter-node': svelteKitAdapterVersion,
+      '@sveltejs/kit': svelteKitVersion,
+      svelte: svelteVersion,
+      'svelte-preprocess': sveltePreprocessVersion,
+    }
+  );
 }
