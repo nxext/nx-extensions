a8e20d7a1cf025b7632f0c7b0176f4b5efae0c85
Jordan Hall <jordan@libertyware.co.uk>
2022-01-27 10:19:28 +0000

    fix: add "@nxext/vite": as dependencies

--- packages/angular/plugins/vite.ts
+++ packages/angular/plugins/vite.ts
@@ -1,13 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const Inspect = require('vite-plugin-inspect');
-
 import { ViteAngularPlugin } from './vite-plugin-angular';
 
 export default defineConfig({
   plugins: [
-    Inspect(),
     ViteAngularPlugin({
       target: 'es2020',
     }),
