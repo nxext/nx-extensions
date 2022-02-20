c0df8e7e0b6cd45abc73b761877c92cc66963b23
Jordan Hall <jordan@libertyware.co.uk>
2022-02-20 11:11:57 +0000

    fix: add migration script
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

---
+++ packages/vite/src/migrations/viteconfig-to-config-file/viteconfig-to-config-file.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getProjects, Tree, updateProjectConfiguration } from '@nrwl/devkit';
+import { join } from 'path';
+
+export default function update(host: Tree) {
+  const projects = getProjects(host);
+
+  for (const [projectName, project] of projects) {
+    let hasChanged = false;
+    for (const [key, config] of Object.entries(project.targets)) {
+      if (
+        config.executor === '@nxext/vite:package' &&
+        !!config.options?.viteConfig
+      ) {
+        hasChanged = true;
+        config.options = {
+          ...config.options,
+          configFile: config.options?.viteConfig,
+        };
+        delete config.options.viteConfig;
+      }
+      return;
+    }
+
+    if (hasChanged) {
+      updateProjectConfiguration(host, projectName, project);
+    }
+  }
+}
