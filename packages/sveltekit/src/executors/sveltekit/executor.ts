97ad656123e6c1bbc456279f5ed80773d5d4b738
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2022-03-24 02:32:36 +0000

    chore(sveltekit): updating executor
    executor now copies package.json to dist properly

--- packages/sveltekit/src/executors/sveltekit/executor.ts
+++ packages/sveltekit/src/executors/sveltekit/executor.ts
@@ -9,20 +9,32 @@ export default async function runExecutor(
   const projectDir = context.workspace.projects[context.projectName].root;
   const projectRoot = joinPathFragments(`${context.root}/${projectDir}`);
   const portOption = options.command === 'dev' ? ` --port ${options.port}` : '';
+  const projectOutput = joinPathFragments(
+    context.root,
+    'dist/packages/',
+    context.projectName
+  );
 
   const result = await runCommands(
     {
+      cwd: projectRoot,
       commands: [
+        {
+          // creates folder if it doesn't exist
+          // otherwhise cp command doesn't work
+          command: `mkdir -p ${projectOutput}`,
+          description: 'Creating output folder',
+        },
         {
           command: 'cp package.json ../../dist/packages/' + context.projectName,
           description: 'coppying the package.json to dist',
         },
+        // svelte-kit on serving, looks to the nearest parent package.json
         {
           command: `svelte-kit ${options.command}${portOption}`,
           description: 'Executing svelte-kit command',
         },
       ],
-      cwd: projectRoot,
       parallel: false,
       color: true,
     },
