ed095ad501cb274b41200aca2d3386deb8fe5761
Dominik Pieper <dominik@pieper.io>
2022-04-15 09:35:40 +0200

    chore(nxext): update Nx and deps

--- packages/svelte/src/generators/utils/versions.ts
+++ packages/svelte/src/generators/utils/versions.ts
@@ -1,8 +1,8 @@
-export const eslintPluginSvelteVersion = '^3.4.0';
-export const svelteVersion = '^3.46.4';
+export const eslintPluginSvelteVersion = '^3.4.1';
+export const svelteVersion = '^3.47.0';
 export const svelteJesterVersion = '^2.3.2';
 export const svelteCheckVersion = '^2.4.5';
-export const sveltePreprocessVersion = '^4.10.3';
+export const sveltePreprocessVersion = '^4.10.6';
 export const tsconfigSvelteVersion = '^3.0.0';
 export const testingLibrarySvelteVersion = '^3.0.3';
 export const vitePluginSvelteVersion = '^1.0.0-next.37';
