0f3c0a22ab2aa6af7ea1f5af50fd00214f94dc10
Dominik Pieper <dominik@pieper.io>
2022-05-03 15:29:41 +0200

    chore(stencil): update stencil dependencies

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,11 +1,11 @@
-export const stencilVersion = '^2.15.0';
+export const stencilVersion = '^2.15.1';
 export const stencilRouterVersion = '^1.0.1';
-export const ionicVersion = '^6.0.16';
+export const ionicVersion = '^6.1.3';
 
 export const autoprefixerVersion = '^9.7.6';
 export const autoprefixerTypesVersion = '^9.7.2';
 
-export const puppeteer = '^13.5.2';
+export const puppeteer = '^13.7.0';
 
 export const eslintPluginImportVersion = '2.22.1';
 export const eslintPluginStencilVersion = '0.3.1';
