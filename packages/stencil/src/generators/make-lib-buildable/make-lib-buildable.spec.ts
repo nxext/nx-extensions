8c80385ad344c5683a7eb3a393f37bdb4df0743f
Marc Kassay <marckassay@gmail.com>
2022-06-09 16:16:54 -0400

    test(stencil): add describe for make-lib-buildable

--- packages/stencil/src/generators/make-lib-buildable/make-lib-buildable.spec.ts
+++ packages/stencil/src/generators/make-lib-buildable/make-lib-buildable.spec.ts
@@ -1,10 +1,16 @@
 import { uniq } from '@nrwl/nx-plugin/testing';
 import { MakeLibBuildableSchema } from './schema';
 import { SupportedStyles } from '../../stencil-core-utils';
-import { readJson, Tree } from '@nrwl/devkit';
+import {
+  readJson,
+  readWorkspaceConfiguration,
+  Tree,
+  updateWorkspaceConfiguration,
+} from '@nrwl/devkit';
 import { makeLibBuildableGenerator } from './make-lib-buildable';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { libraryGenerator } from '../library/generator';
+import { testNpmScope } from '../../utils/testing';
 
 describe('make-lib-buildable schematic', () => {
   let host: Tree;
@@ -66,6 +72,39 @@ describe('make-lib-buildable schematic', () => {
     const tsConfig = readJson(host, 'tsconfig.base.json');
 
     expect(tsConfig.compilerOptions.paths['@my/lib']).toEqual([
+      `dist/libs/${name}`,
+    ]);
+  });
+});
+
+describe('make-lib-buildable schematic using defaults', () => {
+  let host: Tree;
+  const name = uniq('testproject');
+  const options: MakeLibBuildableSchema = {
+    name,
+    style: SupportedStyles.css,
+  };
+
+  beforeEach(async () => {
+    host = createTreeWithEmptyWorkspace();
+    const workspaceConfiguration = readWorkspaceConfiguration(host);
+    workspaceConfiguration.npmScope = testNpmScope;
+    updateWorkspaceConfiguration(host, workspaceConfiguration);
+
+    await libraryGenerator(host, {
+      name: options.name,
+      style: SupportedStyles.css,
+      importPath: options.importPath,
+      buildable: false,
+      publishable: false,
+    });
+  });
+
+  it(`should set default path in tsconfig for buildable libs`, async () => {
+    await makeLibBuildableGenerator(host, options);
+    const tsConfig = readJson(host, 'tsconfig.base.json');
+
+    expect(tsConfig.compilerOptions.paths[`@${testNpmScope}/${name}`]).toEqual([
       `dist/libs/${name}`,
     ]);
   });
