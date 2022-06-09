e1cceb1ef3f22076ae0df288e04be9d778f659d5
Marc Kassay <marckassay@gmail.com>
2022-06-09 14:16:29 -0400

    feat(stencil): importPath optional for make-lib-buildable command

--- packages/stencil/src/generators/make-lib-buildable/schema.d.ts
+++ packages/stencil/src/generators/make-lib-buildable/schema.d.ts
@@ -3,5 +3,11 @@ import { InitSchema } from '../../generators/init/schema';
 
 export interface MakeLibBuildableSchema extends InitSchema {
   style: SupportedStyles;
-  importPath: string
+
+  /**
+   * The TS `paths` entry to be used across Nx projects.
+   *
+   * If no value is set, defaults to Nx workspace name followed by Nx project name.
+   */
+  importPath?: string;
 }
