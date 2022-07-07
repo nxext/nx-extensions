1f240072bd445ffe8076a16ce40ca6db446062a6
Dominik Pieper <dominik@pieper.io>
2022-07-07 16:21:07 +0200

    feat(sveltekit): first change to move sveltekit to new Vite based build

--- packages/sveltekit/src/generators/application/generator.ts
+++ packages/sveltekit/src/generators/application/generator.ts
@@ -2,11 +2,13 @@ import {
   addProjectConfiguration,
   formatFiles,
   generateFiles,
-  getWorkspaceLayout, joinPathFragments,
+  getWorkspaceLayout,
+  joinPathFragments,
   names,
   getWorkspacePath,
   offsetFromRoot,
-  Tree, convertNxGenerator
+  Tree,
+  convertNxGenerator,
 } from '@nrwl/devkit';
 import * as path from 'path';
 import { NormalizedSchema, SveltekitGeneratorSchema } from './schema';
@@ -15,6 +17,8 @@ import { relative } from 'path';
 import { addLinting } from './lib/add-linting';
 import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
 import { installDependencies } from './lib/install-dependencies';
+import { viteInitGenerator } from '@nxext/vite';
+import { createViteTargets } from './lib/targets';
 
 function normalizeOptions(
   host: Tree,
@@ -30,7 +34,10 @@ function normalizeOptions(
   const parsedTags = options.tags
     ? options.tags.split(',').map((s) => s.trim())
     : [];
-  const distDir = relative(joinPathFragments(`${workspacePath}/${projectRoot}`), joinPathFragments(`${workspacePath}/dist/${projectRoot}`));
+  const distDir = relative(
+    joinPathFragments(`${workspacePath}/${projectRoot}`),
+    joinPathFragments(`${workspacePath}/dist/${projectRoot}`)
+  );
 
   return {
     ...options,
@@ -57,41 +64,38 @@ function addFiles(host: Tree, options: NormalizedSchema) {
   );
 }
 
-export async function applicationGenerator(host: Tree, schema: SveltekitGeneratorSchema) {
+export async function applicationGenerator(
+  host: Tree,
+  schema: SveltekitGeneratorSchema
+) {
   const options = normalizeOptions(host, schema);
+
+  const targets = {
+    ...createViteTargets(options),
+    add: {
+      executor: '@nxext/sveltekit:add',
+    },
+  };
+
   addProjectConfiguration(host, options.projectName, {
     root: options.projectRoot,
     projectType: ProjectType.Application,
     sourceRoot: `${options.projectRoot}/src`,
-    targets: {
-      build: {
-        executor: '@nxext/sveltekit:sveltekit',
-        options: {
-          command: 'build'
-        }
-      },
-      serve: {
-        executor: '@nxext/sveltekit:sveltekit',
-        options: {
-          command: 'dev'
-        }
-      },
-      add: {
-        executor: '@nxext/sveltekit:add',
-      }
-    },
+    targets: targets,
     tags: options.parsedTags,
   });
   addFiles(host, options);
   const lintTask = await addLinting(host, options);
 
-  if(!options.skipFormat) {
+  const viteTask = await viteInitGenerator(host, options);
+
+  if (!options.skipFormat) {
     await formatFiles(host);
   }
 
   const installTask = installDependencies(host);
 
-  return runTasksInSerial(installTask, lintTask);
+  return runTasksInSerial(installTask, lintTask, viteTask);
 }
 
 export default applicationGenerator;
