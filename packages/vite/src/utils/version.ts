57a113a323c15a43bcd5360a7e0b6bdbb0c73381
Dominik Pieper <dominik@pieper.io>
2022-07-13 11:10:51 +0200

    chore(nxext): update dependencies

--- packages/vite/src/utils/version.ts
+++ packages/vite/src/utils/version.ts
@@ -1,4 +1,4 @@
-export const viteVersion = '^3.0.0-alpha.13';
+export const viteVersion = '^3.0.2';
 
 export const eslintPluginImportVersion = '^2.26.0';
 export const eslintPluginViteVersion = '^1.6.0';
