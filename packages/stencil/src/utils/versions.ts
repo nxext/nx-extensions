2edf8fc95e71ee604e8f8645762b81fdeca6957e
Dominik Pieper <dominik@pieper.io>
2022-07-13 11:10:51 +0200

    chore(nxext): update dependencies

--- packages/stencil/src/utils/versions.ts
+++ packages/stencil/src/utils/versions.ts
@@ -1,11 +1,11 @@
-export const stencilVersion = '^2.15.2';
+export const stencilVersion = '^2.17.1';
 export const stencilRouterVersion = '^1.0.1';
-export const ionicVersion = '^6.1.5';
+export const ionicVersion = '^6.1.14';
 
 export const autoprefixerVersion = '^9.7.6';
 export const autoprefixerTypesVersion = '^9.7.2';
 
-export const puppeteer = '^13.7.0';
+export const puppeteer = '^15.4.0';
 
 export const eslintPluginImportVersion = '2.22.1';
 export const eslintPluginStencilVersion = '0.3.1';
@@ -15,7 +15,7 @@ export const STENCIL_OUTPUTTARGET_VERSION: {
 } = {
   react: '^0.3.1',
   angular: '^0.4.0',
-  vue: '^0.6.0',
+  vue: '^0.6.1',
   svelte: '^0.0.3',
 };
 
--- packages/vitest/src/utils/versions.ts
+++ packages/vitest/src/utils/versions.ts
@@ -1,2 +1,2 @@
-export const vitestVersion = '^0.12.6';
+export const vitestVersion = '^0.18.0';
 export const c8Version = '^7.11.3';
