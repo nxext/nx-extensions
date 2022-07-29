d29c4c0ec78d51f464fe1ce587ad7ed5821a5c43
Dominik Pieper <dominik@pieper.io>
2022-07-29 13:43:09 +0200

    chore(nxext): update update-playground scripts

--- tools/scripts/update-playground.ts
+++ tools/scripts/update-playground.ts
@@ -1,9 +1,11 @@
 // @ts-ignore
 import { execSync } from 'child_process';
-import { removeSync } from 'fs-extra';
+import { readFileSync, removeSync, writeFileSync } from 'fs-extra';
 import { copyAndRename, getPublishableLibNames, tmpProjPath } from './utils';
 import { Workspaces } from 'nx/src/config/workspaces';
 import { workspaceRoot } from '@nrwl/tao/src/utils/app-root';
+import { logger, readJsonFile } from '@nrwl/devkit';
+import * as glob from 'glob';
 
 console.log('\nUpdating playground...');
 
@@ -20,13 +22,39 @@ execSync(`yarn nx run-many --target build --projects ${publishableLibNames}`);
 removeSync(tmpProjPath('node_modules/@nxext'));
 
 publishableLibNames.forEach((pubLibName) => {
-  const { outputPath, packageJson } =
-    workspaceJson.projects[pubLibName]?.targets?.build.options;
-  const packageName = require(`${workspaceRoot}/${packageJson}`).name;
-  copyAndRename(
-    `${workspaceRoot}/${outputPath}`,
-    tmpProjPath(`node_modules/${packageName}`)
-  );
+  try {
+    logger.info(`Processing ${pubLibName} ...`);
+    const { outputPath, packageJson } =
+      workspaceJson.projects[pubLibName]?.targets?.build.options;
+    const p = JSON.parse(readFileSync(tmpProjPath('package.json')).toString());
+    p.devDependencies[
+      require(`${workspaceRoot}/${packageJson}`).name
+      ] = `file:${workspaceRoot}/${outputPath}`;
+    writeFileSync(tmpProjPath('package.json'), JSON.stringify(p, null, 2));
+  } catch (e) {
+    logger.info(`Problem with ${pubLibName}`);
+  }
+});
+logger.info(`All packages processed.`);
+
+logger.info('Update versions...');
+const tsConfigBase = readJsonFile(`${workspaceRoot}/tsconfig.base.json`);
+const distPaths = Object.entries(tsConfigBase.compilerOptions.paths).reduce<Record<string, string>>((acc, cur) => {
+  acc[cur[0]] = `file:${workspaceRoot}/dist/${cur[1][0].split('/src')[0]}`;
+
+  return acc;
+}, {} as Record<string, string>);
+
+let pkgFiles = glob.sync('dist/packages/**/package.json');
+
+pkgFiles.forEach((p) => {
+  const content = JSON.parse(readFileSync(p).toString());
+  for (const key in content.dependencies) {
+    if (key.startsWith('@nxext/')) {
+      content.dependencies[key] = distPaths[key];
+    }
+  }
+  writeFileSync(p, JSON.stringify(content, null, 2));
 });
 
 console.log('\nUpdate complete.');
