7f163beec0961b601f7cd8de54b75b93a236f7e1
Dominik Pieper <dominik@pieper.io>
2022-07-29 07:13:02 +0200

    chore(nxext): update Nx and other deps

--- packages/ionic-react/src/utils/versions.ts
+++ packages/ionic-react/src/utils/versions.ts
@@ -1,7 +1,6 @@
-export const nxVersion = '^14.4.0';
-export const nxextCapacitorVersion = '^14.0.0';
-export const ionicReactVersion = '^6.1.5';
-export const ionicReactRouterVersion = '^6.1.5';
+export const nxVersion = '^14.5.0';
+export const ionicReactVersion = '^6.2.0';
+export const ionicReactRouterVersion = '^6.2.0';
 export const webVitalsVersion = '^0.2.4';
 export const workboxVersion = '^5.1.4';
 export const reactRouterDomVersion = '5.3.0';
