6d9526c6ee734f2d8edc25b5bd37a4ed51d10ab8
Dominik Pieper <dominik@pieper.io>
2022-08-11 13:17:49 +0200

    chore(nxext): update vite and vite plugins versions

--- packages/solid/src/generators/utils/versions.ts
+++ packages/solid/src/generators/utils/versions.ts
@@ -2,4 +2,4 @@ export const solidJestVersion = '^0.2.0';
 export const solidVersion = '^1.3.3';
 export const solidTestingLibraryVersion = '^0.2.1';
 export const eslintPluginSolidVersion = '0.4.3';
-export const vitePluginSolidVersion = '2.2.6';
+export const vitePluginSolidVersion = '2.3.0';
