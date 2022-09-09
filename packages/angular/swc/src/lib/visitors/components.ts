cd2af372bad17b73c97ec5bf8d7edfdcc8e32eeb
nitedani <67418286+nitedani@users.noreply.github.com>
2022-09-10 01:29:13 +0200

    fix: #804

--- packages/angular/swc/src/lib/visitors/components.ts
+++ packages/angular/swc/src/lib/visitors/components.ts
@@ -105,7 +105,7 @@ export class AngularComponents extends Visitor {
                       return createKeyValueProperty(
                         createIdentifer('template'),
                         createTemplateLiteral([
-                          createTemplateElement(templateContent),
+                          createTemplateElement(templateContent, true),
                         ])
                       );
                     }
