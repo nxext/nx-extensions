60602a4a688fa3a46926e7f10f87fb6b58811018
Jordan Hall <Jordan@lts.sh>
2022-09-30 09:27:15 +0100

    fix install dependcy
    Signed-off-by: Jordan Hall <Jordan@lts.sh>

--- packages/preact/src/generators/utils/versions.ts
+++ packages/preact/src/generators/utils/versions.ts
@@ -1,4 +1,4 @@
 export const preactVersion = '^10.6.4';
 export const testingLibraryPreactVersion = '^2.0.1';
-export const vitePluginPreactVersion = '^2.1.7';
+export const vitePluginPreactVersion = '^2.4.0';
 export const eslintPluginPreactVersion = '^0.1.0';
