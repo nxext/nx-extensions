d54204baba0254c922dbf3f89284d00d8caaf870
Dominik Pieper <dominik@pieper.io>
2022-10-18 07:00:07 +0200

    fix(nxext): generalize e2e test

--- e2e/e2e/src/utils/json-utils.ts
+++ e2e/e2e/src/utils/json-utils.ts
@@ -1,9 +1,10 @@
 import { readFileSync, writeFileSync } from 'fs-extra';
-import { logger } from '@nrwl/devkit';
+import { logger, readNxJson } from '@nrwl/devkit';
 import * as glob from 'glob';
 
 export function updatePackageJsonFiles(version, isLocal) {
   logger.info('Update versions...');
+  const nxJson = readNxJson();
   let pkgFiles = glob.sync('dist/packages/**/package.json');
 
   if (isLocal) {
@@ -13,7 +14,7 @@ export function updatePackageJsonFiles(version, isLocal) {
     const content = JSON.parse(readFileSync(p).toString());
     content.version = version;
     for (const key in content.dependencies) {
-      if (key.startsWith('@nxext/')) {
+      if (key.startsWith(`@${nxJson.npmScope}/`)) {
         content.dependencies[key] = version;
       }
     }
