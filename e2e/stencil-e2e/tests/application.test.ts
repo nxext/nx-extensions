a443fa89e671110a85bb782b854c88d785d88774
Dominik Pieper <dominik@pieper.io>
2023-01-16 07:21:08 +0100

    refactor(stencil): simplify e2e tests

--- e2e/stencil-e2e/tests/application.test.ts
+++ e2e/stencil-e2e/tests/application.test.ts
@@ -19,35 +19,51 @@ describe('application e2e', () => {
     cleanup();
   });
 
-  it(`should build app with css`, async () => {
-    const plugin = uniq('app2');
-    await runNxCommandAsync(
-      `generate @nxext/stencil:app ${plugin} --style='css' --e2eTestRunner='none' --junitTestRunner='none'`
-    );
-
-    const result = await runNxCommandAsync(`build ${plugin} --dev`);
-    expect(result.stdout).toContain('build finished');
-    expect(() => {
-      checkFilesExist(
-        `dist/apps/${plugin}/www/index.html`,
-        `dist/apps/${plugin}/www/host.config.json`
+  describe('app', () => {
+    it(`should build app with css`, async () => {
+      const plugin = uniq('app2');
+      await runNxCommandAsync(
+        `generate @nxext/stencil:app ${plugin} --style='css' --e2eTestRunner='none' --junitTestRunner='none'`
       );
-    }).not.toThrow();
+
+      const result = await runNxCommandAsync(`build ${plugin} --dev`);
+      expect(result.stdout).toContain('build finished');
+      expect(() => {
+        checkFilesExist(
+          `dist/apps/${plugin}/www/index.html`,
+          `dist/apps/${plugin}/www/host.config.json`
+        );
+      }).not.toThrow();
+    });
+
+    it(`should build app with prerender parameter`, async () => {
+      const plugin = uniq('app-prerender');
+      await runNxCommandAsync(
+        `generate @nxext/stencil:app ${plugin} --style='css' --e2eTestRunner='none' --junitTestRunner='none'`
+      );
+
+      const result = await runNxCommandAsync(
+        `build ${plugin} --prerender=true`
+      );
+      expect(result.stdout).toContain('build finished');
+      expect(() => {
+        checkFilesExist(
+          `dist/apps/${plugin}/www/index.html`,
+          `dist/apps/${plugin}/www/host.config.json`
+        );
+      }).not.toThrow();
+    });
   });
 
-  it(`should build app with prerender parameter`, async () => {
-    const plugin = uniq('app-prerender');
-    await runNxCommandAsync(
-      `generate @nxext/stencil:app ${plugin} --style='css' --e2eTestRunner='none' --junitTestRunner='none'`
-    );
-
-    const result = await runNxCommandAsync(`build ${plugin} --prerender=true`);
-    expect(result.stdout).toContain('build finished');
-    expect(() => {
-      checkFilesExist(
-        `dist/apps/${plugin}/www/index.html`,
-        `dist/apps/${plugin}/www/host.config.json`
+  describe('pwa', () => {
+    it(`should build pwa app with scss`, async () => {
+      const plugin = uniq('pwa');
+      await runNxCommandAsync(
+        `generate @nxext/stencil:pwa ${plugin} --style='scss' --e2eTestRunner='none' --junitTestRunner='none'`
       );
-    }).not.toThrow();
+
+      const result = await runNxCommandAsync(`build ${plugin} --dev`);
+      expect(result.stdout).toContain('build finished');
+    });
   });
 });
