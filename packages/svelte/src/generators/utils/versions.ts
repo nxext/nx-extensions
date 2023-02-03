822cc508ebead333ac158d8a1bd6837151b6af6d
Dominik Pieper <dominik@pieper.io>
2023-02-03 07:09:08 +0100

    chore(nxext): update dependencies

--- packages/svelte/src/generators/utils/versions.ts
+++ packages/svelte/src/generators/utils/versions.ts
@@ -1,9 +1,9 @@
 export const eslintPluginSvelteVersion = '^4.0.0';
-export const svelteVersion = '^3.55.0';
+export const svelteVersion = '^3.55.1';
 export const svelteJesterVersion = '^2.3.2';
 export const svelteCheckVersion = '^2.10.2';
 export const svelteLoaderVersion = '^3.1.2';
-export const sveltePreprocessVersion = '^5.0.0';
+export const sveltePreprocessVersion = '^5.0.1';
 export const tsconfigSvelteVersion = '^3.0.0';
 export const testingLibrarySvelteVersion = '^3.2.2';
 export const vitePluginSvelteVersion = '^1.0.1';
