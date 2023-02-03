7d6a88ce8cf250e7214123f6a512f381181e8b92
Dominik Pieper <dominik@pieper.io>
2023-02-03 07:09:08 +0100

    chore(nxext): update dependencies

--- packages/solid/src/generators/utils/versions.ts
+++ packages/solid/src/generators/utils/versions.ts
@@ -1,5 +1,5 @@
 export const solidJestVersion = '^0.2.0';
-export const solidVersion = '^1.3.3';
-export const solidTestingLibraryVersion = '^0.2.1';
-export const eslintPluginSolidVersion = '0.4.3';
-export const vitePluginSolidVersion = '2.3.10';
+export const solidVersion = '^1.6.10';
+export const solidTestingLibraryVersion = '^0.5.1';
+export const eslintPluginSolidVersion = '0.9.4';
+export const vitePluginSolidVersion = '2.5.0';
