dd48a3cfdfea5f984d92eab37dbe3c938b0693b8
Dominik Pieper <dominik@pieper.io>
2023-03-15 06:32:09 +0100

    chore(react): move react package to archive

--- packages/react/src/generators/utils/versions.ts
+++
@@ -1,1 +0,0 @@
-export const vitePluginReactVersion = '^2.1.0';
--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './generators/init/init';
-export * from './generators/library/library';
-export * from './generators/application/application';
