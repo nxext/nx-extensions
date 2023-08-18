86f3a53eb8e5758eac903ec1b53f74487c1d3192
Dominik Pieper <dominik@pieper.io>
2023-08-18 06:51:14 +0200

    fix(vue): fix vue build

--- packages/vue/src/generators/utils/versions.ts
+++ packages/vue/src/generators/utils/versions.ts
@@ -1,8 +1,8 @@
 export const vueVersion = '^3.3.4';
-export const vueTscVersion = '^1.6.5';
-export const vueRouterVersion = '^4.2.2';
-export const vitePluginVueVersion = '^4.2.3';
-export const vueTestUtilsVersion = '^2.3.2';
+export const vueTscVersion = '^1.8.8';
+export const vueRouterVersion = '^4.2.4';
+export const vitePluginVueVersion = '^4.3.1';
+export const vueTestUtilsVersion = '^2.4.1';
 export const eslintPluginVueVersion = '^9.14.1';
-export const eslintPluginPrettierVueVersion = '^7.1.0';
+export const eslintPluginPrettierVueVersion = '^8.0.0';
 export const eslintPluginTypescriptVueVersion = '^11.0.3';
