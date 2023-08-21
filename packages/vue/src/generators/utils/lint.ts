ef82b866bf2c072645d7d373107cdbde07e591a3
Dominik Pieper <dominik@pieper.io>
2023-08-21 08:34:20 +0200

    fix(vue): fix Vue linting

--- packages/vue/src/generators/utils/lint.ts
+++ packages/vue/src/generators/utils/lint.ts
@@ -21,7 +21,7 @@ export const extendVueEslintJson = (json: any) => {
       'plugin:vue/vue3-essential',
       'eslint:recommended',
       '@vue/eslint-config-typescript',
-      '@vue/eslint-config-prettier',
+      '@vue/eslint-config-prettier/skip-formatting',
       ...(pluginExtends || []),
     ],
     ...config,
