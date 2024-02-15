2337392df00f4e51232d8e2889076d10e998cafb
Sambath Kumar Logakrishnan <6957824+64BitAsura@users.noreply.github.com>
2024-02-15 15:03:15 +0000

    fix(stencil): Don't look for dependencies on same targetName when dependsOn is specified

--- packages/stencil/src/executors/build/executor.ts
+++ packages/stencil/src/executors/build/executor.ts
@@ -54,6 +54,7 @@ export default async function runExecutor(
   );
 
   if (
+    !context.target.dependsOn &&
     !checkDependentProjectsHaveBeenBuilt(
       context.root,
       context.projectName,
