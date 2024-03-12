b6449fa2737706a616cf5592d56ebcd4141c2162
Dominik Pieper <dominik@pieper.io>
2024-03-12 16:42:01 +0100

    chore(nxext): update package versions

--- packages/preact/src/generators/utils/versions.ts
+++ packages/preact/src/generators/utils/versions.ts
@@ -1,4 +1,4 @@
 export const preactVersion = '^10.14.0';
 export const testingLibraryPreactVersion = '^3.2.3';
-export const vitePluginPreactVersion = '^2.5.0';
+export const vitePluginPreactVersion = '^2.8.1';
 export const eslintPluginPreactVersion = '^0.1.0';
