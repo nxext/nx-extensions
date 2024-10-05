2c0875075cda22451237cda4d74e0aa434f763cb
Edouard Bozon <bozonedouard@gmail.com>
2024-10-05 14:01:45 +0200

    fix(ionic-react): fix build target + fix e2e tests

--- packages/ionic-react/src/generators/configuration/lib/add-dependencies.ts
+++ packages/ionic-react/src/generators/configuration/lib/add-dependencies.ts
@@ -3,6 +3,8 @@ import {
   ionicReactRouterVersion,
   ionicReactVersion,
   ionIconsVersion,
+  reactRouterVersion,
+  reactVersion,
 } from '../../../utils/versions';
 
 export function addDependencies(host: Tree) {
@@ -12,7 +14,25 @@ export function addDependencies(host: Tree) {
       '@ionic/react': ionicReactVersion,
       '@ionic/react-router': ionicReactRouterVersion,
       ionicons: ionIconsVersion,
+
+      '@capacitor/haptics': '6.0.1',
+      '@capacitor/keyboard': '6.0.2',
+      '@capacitor/status-bar': '6.0.1',
+
+      /**
+       * Following deps are overwrite because React Router v6 is not compatible with Ionic React Router
+       */
+
+      react: '^18.2.0',
+      'react-dom': '^18.2.0',
+      'react-router': '^5.3.4',
+      'react-router-dom': '^5.3.4',
     },
-    {}
+    {
+      '@types/react-router': '^5.1.20',
+      '@types/react-router-dom': '^5.3.3',
+      '@types/react': '^18.0.27',
+      '@types/react-dom': '^18.0.10',
+    }
   );
 }
--- packages/ionic-react/src/utils/versions.ts
+++ packages/ionic-react/src/utils/versions.ts
@@ -1,3 +1,9 @@
 export const ionicReactVersion = '^8.0.0';
 export const ionicReactRouterVersion = '^8.0.0';
 export const ionIconsVersion = '^7.0.0';
+/**
+ * React Router v6 is not compatible with Ionic React Router, so we force using the v5
+ * See: https://github.com/ionic-team/ionic-framework/issues/24177, https://github.com/ionic-team/ionic-framework/blob/main/packages/react-router/package.json#L46
+ */
+export const reactRouterVersion = '^5.3.4';
+export const reactVersion = '^18.2.0';
