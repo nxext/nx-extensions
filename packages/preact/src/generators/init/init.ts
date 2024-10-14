b04b4f2edf6a50cbd2fa740c090ae0c73b2d5a2d
Paweł Twardziak <pawel.twardziak.dev@gmail.com>
2024-10-14 19:54:00 +0200

    chore(preact): add assert-not-using-ts-solution-setup to the generators

--- packages/preact/src/generators/init/init.ts
+++ packages/preact/src/generators/init/init.ts
@@ -3,8 +3,11 @@ import { formatFiles, Tree, runTasksInSerial } from '@nx/devkit';
 import { addJestPlugin } from './lib/add-jest-plugin';
 import { addCypressPlugin } from './lib/add-cypress-plugin';
 import { updateDependencies } from './lib/add-dependencies';
+import { assertNotUsingTsSolutionSetup } from '@nx/js/src/utils/typescript/ts-solution-setup';
 
 export async function initGenerator(host: Tree, options: Schema) {
+  assertNotUsingTsSolutionSetup(host, '@nxext/preact', 'init');
+
   const installTask = updateDependencies(host);
   const jestTask = await addJestPlugin(host, options);
   const cypressTask = await addCypressPlugin(host, options);
