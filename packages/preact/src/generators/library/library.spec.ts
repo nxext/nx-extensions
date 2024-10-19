69155008208fcfcfde8e6bd8d89b5deae477299c
Paweł Twardziak <pawel.twardziak.dev@gmail.com>
2024-10-20 01:34:35 +0200

    test(preact): repair tests

--- packages/preact/src/generators/library/library.spec.ts
+++ packages/preact/src/generators/library/library.spec.ts
@@ -12,28 +12,49 @@ describe('preact library schematic', () => {
     unitTestRunner: 'jest',
     e2eTestRunner: 'cypress',
     skipFormat: false,
+    buildable: true,
   };
 
   beforeEach(() => {
     host = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
   });
 
-  it('should add preact dependencies', async () => {
-    await libraryGenerator(host, options);
-    const packageJson = readJson(host, 'package.json');
-
-    expect(packageJson.devDependencies['preact']).toBeDefined();
-  });
-
   it('should add preact project files', async () => {
     await libraryGenerator(host, options);
-
-    // expect(tree.exists(`libs/${options.name}/preact.config.cjs`)).toBeTruthy();
+    expect(host.exists(`libs/test/project.json`)).toBeTruthy();
+    expect(host.exists(`libs/test/package.json`)).toBeTruthy();
     expect(host.exists(`libs/test/tsconfig.lib.json`)).toBeTruthy();
     expect(host.exists(`libs/test/tsconfig.spec.json`)).toBeTruthy();
     expect(host.exists(`libs/test/tsconfig.json`)).toBeTruthy();
+  });
+
+  it('should not add package.json file when not buildable and publishable', async () => {
+    await libraryGenerator(host, {
+      ...options,
+      buildable: false,
+      publishable: false,
+    });
+    expect(host.exists(`libs/test/package.json`)).toBeFalsy();
+  });
+
+  it('should add preact dependencies', async () => {
+    await libraryGenerator(host, options);
+    const packageJson = readJson(host, 'package.json');
+    expect(packageJson.devDependencies['preact']).toBeDefined();
+  });
+
+  it('should add lint config file', async () => {
+    await libraryGenerator(host, options);
+    expect(host.exists(`libs/test/eslint.config.js`)).toBeFalsy();
+    expect(host.exists(`libs/test/.eslintrc.json`)).toBeTruthy();
+  });
+
+  it('should add lint config file for the flat config', async () => {
+    process.env.ESLINT_USE_FLAT_CONFIG = 'true';
+    await libraryGenerator(host, options);
+    expect(host.exists(`libs/test/eslint.config.js`)).toBeTruthy();
     expect(host.exists(`libs/test/.eslintrc.json`)).toBeFalsy();
-    expect(host.exists(`libs/test/.eslintrc.js`)).toBeTruthy();
+    delete process.env.ESLINT_USE_FLAT_CONFIG;
   });
 
   it('should fail if no importPath is provided with publishable', async () => {
